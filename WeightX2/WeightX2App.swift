f0505bc6235e1325bcfbb096d685a947a4b41a74
k0v0354 <keerthanaa.vm@walamrt.com>
2023-08-13 17:05:29 +0530

    Initial Commit

---
+++ WeightX2/WeightX2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeightX2App.swift
+//  WeightX2
+//
+//  Created by Keerthanaa Vm on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WeightX2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
